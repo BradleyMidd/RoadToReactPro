c0de4b806846d2df24cc5c1dbdc01dfcab4de1c8
brad <bradleymiddelijn1@gmail.com>
2022-09-25 22:57:10 +0200

    Playing with app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Hello World",
+        list = ["Apple", "Pear", "Lemon"],
+        items = list.map((item) => 
+          <li key={item}>{item}</li>
+        ),
+        person = {name: "Bradley", age: 21, born: "The Netherlands"};
+  function intro({name, age, born}){
+          return `My name is ${name} and i am ${age} years old from ${born}`
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
+      <p>This is my shopping list: {list.join(' ')}</p>
+      <p>This is who i am: {intro(person)}</p>
+      <ul>{items}</ul>
     </div>
   );
 }
